801ff92ec51b2bd15e88ab2f0d49057ec3643290
Marino Meneghel <marino.meneghel@xpeppers.com>
2017-02-20 14:51:35 +0100

    Extracted methods to getNumbers from string and sumNumbers list

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,28 @@
-/**
- * Created by marino on 2/20/17.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator {
 
     public int add(String numbers) {
         if (numbers.isEmpty())
             return 0;
-        String nums[] = numbers.split(",");
 
+        return sumNumbers(getNumbers(numbers));
+    }
+
+    private int sumNumbers(List<Integer> numbers) {
         int result = 0;
-        for(int i=0; i<nums.length; i++) {
-            result += Integer.valueOf(nums[i]);
-        }
+        for(Integer num : numbers)
+            result += num;
         return result;
+    }
+
+    private List<Integer> getNumbers(String numbers) {
+        List<Integer> numbersList = new ArrayList<Integer>();
+        String[] stringNumbers = numbers.split(",");
+        for(String num : stringNumbers) {
+            numbersList.add(Integer.valueOf(num));
+        }
+        return numbersList;
     }
 }
