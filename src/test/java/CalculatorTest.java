b732db66ea85b29103eaecee0f66a4da754a22ff
Marino Meneghel <marino.meneghel@xpeppers.com>
2017-02-20 14:55:19 +0100

    Handle any number of input parameters

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,24 +1,34 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CalculatorTest {
 
+    private Calculator calculator;
+
+    @Before
+    public void setUp() throws Exception {
+        calculator = new Calculator();
+    }
+
     @Test
     public void withEmptyStringReturnsZero() throws Exception {
-        Calculator calculator = new Calculator();
         assertEquals(0, calculator.add(""));
     }
 
     @Test
     public void withOneNumberReturnsItsIntValue() throws Exception {
-        Calculator calculator = new Calculator();
         assertEquals(5, calculator.add("5"));
     }
 
     @Test
     public void withTwoNumbersReturnsTheirSum() throws Exception {
-        Calculator calculator = new Calculator();
         assertEquals(5, calculator.add("2,3"));
+    }
+
+    @Test
+    public void withAnyNumbersAmountReturnTheirSum() throws Exception {
+        assertEquals(10, calculator.add("3,2,4,1"));
     }
 }
